0d5d55e2e6349eda1d011eddb83c440733e9433d
Sakthi108 <sakthi1996mech@gmail.com>
2022-02-05 20:34:46 +0530

    task

---
+++ src/org/task/SampleGit.java
@@ -0,0 +1,18 @@
+package org.task;
+
+public class SampleGit {
+	private void add() {
+		int a = 10;
+		int b = 20;
+		
+		int c= a+b;
+		
+		System.out.println("c valuve : "+c);
+
+	}
+	public static void main(String[] args) {
+		SampleGit git = new SampleGit();
+		git.add();
+	}
+
+}
